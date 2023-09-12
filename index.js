021c167bf5d2d3688cfb0d1bd09faaf84b0961a2
Vikesh <vikesh@indigitalit.com>
2023-09-12 17:07:19 +0530

    --update2--

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import multer from 'multer';
-import { createFFmpeg } from '@ffmpeg/ffmpeg';
 
+const express = require('express');
+const cors = require ('cors');
+const multer = require('multer');
+const {createFFmpeg} = require ('@ffmpeg/ffmpeg');
 
 
 const ffmpegInstance = createFFmpeg({ log: true });
@@ -21,14 +21,13 @@ const app = express();
 app.use(cors());
 app.set('view engine', 'ejs');
 
-// app.use((req, res, next) => {
-//     console.log('start---')
-//     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    next();
+  });
 
 
-const port = 3000;
+const port = 8085;
 
 const upload = multer({
     storage: multer.memoryStorage(),
@@ -36,61 +35,47 @@ const upload = multer({
 });
 
 app.get('/',async(req,res)=>{
-
     try{
-        console.log('get----')
-      res.render('./views/test.ejs')
+        res.render('index.ejs', { message: 'Hello, EJS!' });
     }catch(err){
         res.send({err})
     }
 })
 
-app.post('/thumbnail', upload.array('image'), async (req, res) => {
-
+app.post('/', upload.array('image'), async(req, res) => {
 
     try {
         const videoData = req.files.buffer;
+        console.log({videoData});
 
         const ffmpeg = await getFFmpeg();
-
 
         const inputFileName = `input-video`;
         const outputFileName = `output.mp4`;
         let outputData = null;
 
-        let {files} = req;
+        let { files } = req;
 
-        console.log(files)
-        
-        files.forEach((file,i)=>{
-            console.log(file.buffer,'-------',i)
+
+        files.forEach((file, i) => {
             ffmpeg.FS('writeFile', `inputFileName${i}`, file.buffer);
         })
 
-   
-       // ffmpeg.FS('writeFile', inputFileName, videoData);
+
+        // ffmpeg.FS('writeFile', inputFileName, videoData);
 
         await ffmpeg.run(
-            "-loop", "1" ,"-t", "5",
+            "-loop", "1", "-t", "5",
             "-i",
             "inputFileName0",
-            "-loop", "1" ,"-t", "5",
+            "-loop", "1", "-t", "5",
             "-i",
             "inputFileName1",
-            "-loop", "1" ,"-t", "5",
-            "-i",
-            "inputFileName2",
-            "-loop", "1" ,"-t", "5",
-            "-i",
-            "inputFileName3",
-            "-loop", "1" ,"-t", "5",
-            "-i",
-            "inputFileName4",
             "-filter_complex",
-            "[0:v]scale=480:800[s0];[1:v]scale=480:800[s1];[2:v]scale=480:800[s2];[3:v]scale=480:800[s3];[4:v]scale=480:800[s4];[s0][s1]xfade=transition=fade:duration=0.5:offset=2.5[f0];[f0][s2]xfade=transition=fade:duration=0.5:offset=5[f1];[f1][s3]xfade=transition=pixelize:duration=0.5:offset=7.5[f2];[f2][s4]xfade=transition=circleopen:duration=0.5:offset=10[f3]",
-            "-map", "[f3]","-r", "24", "-pix_fmt", "yuv420p","-vcodec", "libx264",
-            outputFileName
-       );
+            "[0:v]scale=480:800[s0];[1:v]scale=480:800[s1];[s0][s1]xfade=transition=fade:duration=0.5:offset=2.5[f3]",
+            "-map", "[f3]", "-r", "24", "-pix_fmt", "yuv420p", "-vcodec", "libx264",
+             outputFileName
+        );
 
         outputData = ffmpeg.FS('readFile', outputFileName);
         // ffmpeg.FS('unlink', inputFileName);
@@ -110,8 +95,6 @@ app.post('/thumbnail', upload.array('image'), async (req, res) => {
     }
 
 });
-
-
 
 
 app.listen(port, () => {
